823d1fa5cfc6654e5580040782b8aeb74c6e5c8b
Eduardo Oliveira <santos.edu3301@gmail.com>
2021-04-23 17:37:19 -0300

    addTask - loadTask
    Adicionei a funcionalidade de adicionar as tasks e de carregá-las na tela.

--- script/script.js
+++ script/script.js
@@ -2,23 +2,47 @@ let input = document.getElementById("toDo");
 let addBtn = document.getElementById("add");
 let deleteBtn = document.getElementById("delete");
 let clearBtn = document.getElementById("clear")
+let viewTasks = document.getElementById("taskList")
 let tasks = {tasks: []}
 
+
+function loadTasksOnScreen() {
+    let taskParsed = JSON.parse(localStorage.getItem("allTasks"))
+
+    if(taskParsed !== null) {
+        for(let i = 0; i < taskParsed.tasks.length; i++) {
+            viewTasks.innerHTML += `<li><input type="checkbox">${taskParsed.tasks[i]}</li>`
+            
+        }
+    }
+
+}
+
+
+
+
+
 function insertTask() {
+
+    viewTasks.innerHTML += `<li><input type="checkbox"> ${input.value}</li>`
+
     if(input.value == '') {
         alert("Por favor, escreva uma tarefa para poder adicioná-la!!")
-    } else {
-
-        if(tasks.tasks.length == 0) {
-            tasks.tasks.push(input.value)
-            localStorage()
-        }
-
-        else {
-
-        }
-
-
     }
-    
+     else {
+
+        if(localStorage.getItem("allTasks") == null) {
+            tasks.tasks.push(input.value)
+            localStorage.setItem("allTasks", JSON.stringify(tasks))
+            // console.log(localStorage.getItem("allTasks"))
+        }
+        else {
+            let tasksParsed = JSON.parse(localStorage.getItem("allTasks"))
+            tasksParsed.tasks.push(input.value)
+            localStorage.setItem("allTasks", JSON.stringify(tasksParsed))
+        }
+    }
+
+    // console.log(localStorage.getItem("allTasks"))
+
 }
